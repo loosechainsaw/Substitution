c84adff3150a49323d2c5e474c4b8c9cbc1e6608
Blair Davidson <blaird@signiq.com>
2016-11-16 16:31:07 +0800

    Use using statement as expressions

---
+++ Substitution/Using.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Substitution
+{
+    public static class Using
+    {
+        public static UsingExpression<T> From<T>(Func<T> f) where T : IDisposable => new UsingExpression<T>(f);
+    }
+}
---
+++ Substitution/UsingExpression.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Substitution
+{
+    public class UsingExpression<T>
+        where T : IDisposable
+    {
+        public UsingExpression(Func<T> f)
+        {
+            _f = f;
+        }
+
+        public U Yield<U>(Func<T, U> accessor)
+        {
+            using (var disposable = _f())
+            {
+                return accessor(disposable);
+            }
+        }
+
+        public void ExecuteSideEffect(Action<T> process)
+        {
+            using (var disposable = _f())
+                process(disposable);
+        }
+
+        private readonly Func<T> _f;
+
+    }
+}
